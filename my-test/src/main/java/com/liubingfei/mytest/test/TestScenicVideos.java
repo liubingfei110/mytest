c63468bd856f7ba8bc3d8986778fb0a652e880c3
liubingfei <liubingfei110@163.com>
2024-08-09 17:30:44 +0800

    调用接口查询视频和人流量数据

--- my-test/src/main/java/com/liubingfei/mytest/test/TestScenicVideos.java
+++ my-test/src/main/java/com/liubingfei/mytest/test/TestScenicVideos.java
@@ -138,11 +138,22 @@ public class TestScenicVideos {
     /**
      * 4. 查询视频流地址 传参：监控点唯一标识：indexCode=b99814f234854a84b28ef24f4ede794e，获取url
      * 参数：
-     * 1：b99814f234854a84b28ef24f4ede794e
-     * 2：46444053c14d4673a42a0d7db1b0d9c3
-     * 3：5654941ce1024030ad4e6ff7c54e6e1e
-     * 4：4e56190738314ea99d3e53cde1231d31
-     * ......
+     * 1：b99814f234854a84b28ef24f4ede794e    大七孔左栈道悬崖侧枪机
+     * 2：46444053c14d4673a42a0d7db1b0d9c3    小七孔东门入口_客流统计
+     * 3：5654941ce1024030ad4e6ff7c54e6e1e    小七孔东门出口_客流统计
+     * 4：4e56190738314ea99d3e53cde1231d31    小七孔桥球机右
+     * 5：ee998fa2eaeb4b29b2870704fc57481e    新西门全景外
+     * 6：38a5b3bbfb464fd5a9f3fe5c16c1434a    新西门全景外球机（重点）
+     * 7：cba2d05b463045818a3a39d81f2e7de8    西门咨询台
+     * 8：e5702161cc364fbb9346256953599f13    西门入口长廊枪机
+     * 9：c220d6c8fc1e4d38a4d90d454bb36b6d    大七孔LED屏前
+     * 10：ff0c4d61a7ce43cea98cecb9d8dd9bbf   大七孔候车亭全景
+     * 11：496c1abc742a4522b51edbe596f07cbf   新西门出口闸机人脸1
+     * 12：60197f0afe774a0499bdc95f05876586   拉关湖左栈道球机
+     * 13：e22018a00975401b9836757c744c23b5   大七孔新桥头球机
+     * 14：35538ed829df464284d50c0427ae52d6   大七孔候车亭全景球机
+     * 15: 92f0f70f0bef471180f3dba2748c8d29   努内吉海左栈道枪机
+     * 16: f171e56490964ce3a3877e16c13c9849   上己定湖VIP休息室枪机2
      */
     @Test
     public void callAndGetVideoDetail(){
@@ -152,7 +163,7 @@ public class TestScenicVideos {
         String secret = "G77MGfQ9peH6HtiyLV4P";
         String methodStr = "/api/video/v2/cameras/previewURLs";
         JSONObject params = new JSONObject();
-        params.put("cameraIndexCode", "b99814f234854a84b28ef24f4ede794e");
+        params.put("cameraIndexCode", "f171e56490964ce3a3877e16c13c9849");
         params.put("streamform", "ps");
         params.put("streamType", "0");
         params.put("transmode", "1");
